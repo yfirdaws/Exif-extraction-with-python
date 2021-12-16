8ee7d4fb4c37ca3f22d25a156dffb5bae04e3fed
Firdaws EleOjo Yahya <yfirdaws@gmail.com>
2021-12-16 15:14:47 +0100

    commit

--- draft.py
+++ draft.py
@@ -1,6 +1,4 @@
-
 import pandas as pd
-#import numpy as np 
 from pathlib import Path
 from PIL import Image
 from PIL.ExifTags import TAGS
@@ -10,26 +8,34 @@ import os
 
 #loading files
 for f in glob.glob(r'\\nagisfs03\per\Firdaws.Yahya\shared\NS 40004/*.JPG'): 
-  fle=f #replace with filename
+  head, tail = os.path.split(f)
+  fle=tail #replace with filename
   filename=os.path.splitext(fle)[0] #remove.jpg from file name
   im=Image.open(fle)
  
   # dictionary to store metadata keys and value pairs.
   exif = {}
 
+  imm=im._getexif()
+
   # iterating over the dictionary 
-  for tag, value in im._getexif().items():
+  if imm:
+    for tag, value in imm.items():
+      
+    #extarcting all the metadata as key and value pairs and converting them from numerical value to string values
+        if tag in TAGS:
+            exif[TAGS[tag]] = value  
+    print()
+    print(filename)
+    print("Displaying all the metadatas of the image: \n")
+    print(exif)
+  else:
+      print(filename)
+      print("Sorry, image has no exif data.")
 
-  #extarcting all the metadata as key and value pairs and converting them from numerical value to string values
-      if tag in TAGS:
-          exif[TAGS[tag]] = value  
-
-  print()
-  print("Displaying all the metadatas of the image: \n")
-  print(exif)
 #creating dataframe
-df=pd.DataFrame([exif])
+#df=pd.DataFrame([exif])
 #inserting filename to first column
-df.insert(0,'Image',filename)
-df.head()
+#df.insert(0,'Image',filename)
+#df.head()
 #df.to_csv('Exif data.csv')
